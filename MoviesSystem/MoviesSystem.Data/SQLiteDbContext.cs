0f94a46203ce9b0fc35074b543343ae8a1056268
SimonaArsova <simona.liubomirova@gmail.com>
2017-06-13 09:23:07 +0300

    Add SQLite migration

---
+++ MoviesSystem/MoviesSystem.Data/SQLiteDbContext.cs
@@ -0,0 +1,32 @@
+﻿using MoviesSystem.Data.SQLiteMigrations;
+using MoviesSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesSystem.Data
+{
+    public class SQLiteDbContext : DbContext
+    {
+        public SQLiteDbContext() : base("SQLiteConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<SQLiteDbContext, SQLiteConfigurations>(true));
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Description> Description { get; set; }
+
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MoviesSystem/MoviesSystem.Data/SQLiteMigrations/201706121954175_InitializeModels.cs
@@ -0,0 +1,114 @@
+namespace MoviesSystem.Data.SQLiteMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitializeModels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Actors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 30),
+                        LastName = c.String(nullable: false, maxLength: 30),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 50),
+                        Description_Id = c.Int(),
+                        Rate_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Descriptions", t => t.Description_Id)
+                .ForeignKey("dbo.Rates", t => t.Rate_Id)
+                .Index(t => t.Description_Id)
+                .Index(t => t.Rate_Id);
+            
+            CreateTable(
+                "dbo.Descriptions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Summary = c.String(unicode: false, storeType: "text"),
+                        Year = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Rates",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        RateValue = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.MovieActors",
+                c => new
+                    {
+                        Movie_Id = c.Int(nullable: false),
+                        Actor_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Movie_Id, t.Actor_Id })
+                .ForeignKey("dbo.Movies", t => t.Movie_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Actors", t => t.Actor_Id, cascadeDelete: true)
+                .Index(t => t.Movie_Id)
+                .Index(t => t.Actor_Id);
+            
+            CreateTable(
+                "dbo.GenreMovies",
+                c => new
+                    {
+                        Genre_Id = c.Int(nullable: false),
+                        Movie_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Genre_Id, t.Movie_Id })
+                .ForeignKey("dbo.Genres", t => t.Genre_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Movies", t => t.Movie_Id, cascadeDelete: true)
+                .Index(t => t.Genre_Id)
+                .Index(t => t.Movie_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "Rate_Id", "dbo.Rates");
+            DropForeignKey("dbo.GenreMovies", "Movie_Id", "dbo.Movies");
+            DropForeignKey("dbo.GenreMovies", "Genre_Id", "dbo.Genres");
+            DropForeignKey("dbo.Movies", "Description_Id", "dbo.Descriptions");
+            DropForeignKey("dbo.MovieActors", "Actor_Id", "dbo.Actors");
+            DropForeignKey("dbo.MovieActors", "Movie_Id", "dbo.Movies");
+            DropIndex("dbo.GenreMovies", new[] { "Movie_Id" });
+            DropIndex("dbo.GenreMovies", new[] { "Genre_Id" });
+            DropIndex("dbo.MovieActors", new[] { "Actor_Id" });
+            DropIndex("dbo.MovieActors", new[] { "Movie_Id" });
+            DropIndex("dbo.Movies", new[] { "Rate_Id" });
+            DropIndex("dbo.Movies", new[] { "Description_Id" });
+            DropTable("dbo.GenreMovies");
+            DropTable("dbo.MovieActors");
+            DropTable("dbo.Rates");
+            DropTable("dbo.Genres");
+            DropTable("dbo.Descriptions");
+            DropTable("dbo.Movies");
+            DropTable("dbo.Actors");
+        }
+    }
+}
